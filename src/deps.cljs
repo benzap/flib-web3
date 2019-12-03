d0d628d2bca914e2d56091e0e4b0265d57f9724f
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-12-03 15:01:12 -0500

    Fixing cljs deps for global exports

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,4 +1,5 @@
 {:foreign-libs
  [{:file "flib/web3/web3.js",
-   :provides ["flib.web3"]}],
+   :provides ["flib.web3" "web3"]
+   :global-exports '{web3 Web3}}]
  :externs ["flib/web3/web3.ext.js"]}
